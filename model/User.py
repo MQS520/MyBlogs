d77de9e891b53a839968306dd5cfc7f5426700db
MQS <422933159@qq.com>
2018-12-29 23:37:57 +0800

    createtime的默认值 datetime.datetime.now

--- model/User.py
+++ model/User.py
@@ -5,6 +5,7 @@
 # software: PyCharm
 # desc:
 from app import db
+import datetime
 
 # user表
 class User(db.Model):
@@ -13,11 +14,10 @@ class User(db.Model):
     username = db.Column(db.String(64),index=True,unique=True)
     email = db.Column(db.String(120),index=True,unique=True)
     password = db.Column(db.String(128))
-    createtime = db.Column(db.TIMESTAMP)
+    createtime = db.Column(db.DateTime, default=datetime.datetime.now)
 
     # 数据库中 id自增长，createtime 默认当前时间
     def __init__(self, username, email, password):
         self.username = username
         self.password = password
         self.email = email
-
