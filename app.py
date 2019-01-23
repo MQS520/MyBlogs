9d69a49e0473415b2caa4a38b8240f4be9212457
MQS <15207274447@163.com>
2019-01-24 02:00:15 +0800

    支持接口跨域请求

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 # from flask_migrate import Manager
 
 
 
 
 app = Flask(__name__)
+
+CORS(app, supports_credentials = True)
 
 # 导入配置文件
 app.config.from_object(Config)
